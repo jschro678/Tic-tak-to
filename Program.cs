38c1d33ffff2a1d9f14ebc182e46b09d663cda92
FIRST_NAME LAST_NAME <jschro678@gmail.com>
2022-04-29 23:36:17 -0600

    finished tic tac to

--- Program.cs
+++ Program.cs
@@ -8,13 +8,35 @@ namespace Tic_Tac_To // Jared Schroeder
         {
             List<char>board = new List<char>{'1','2','3','4','5','6','7','8','9'};
             displayboard(board);
-            int i= 0;
-            while (i !=4)
+           int winner = 0;
+            while (winner ==0)
             {
-            player1turn(board);
-            player2turn(board);
-            i++;
-            }
+                player1turn(board);
+               winner= checkWinner(board);
+                if (winner ==1)
+                {
+                    Console.WriteLine("Congrats player 1 you won");
+                }
+                else if (winner ==0)
+                {
+                int tie = checktie(board);
+                if (tie ==0)
+                {
+                player2turn(board);
+                winner=checkWinner(board);
+                if (winner ==1)
+                {
+                    Console.WriteLine("Congrats player 2 you won");
+                }
+                }
+                else if (tie ==1)
+                {
+                    Console.WriteLine("It is a tie");
+                    winner = 1;
+                }
+                }
+            } 
+            
         }
     static void displayboard(List<char>board)
 {
@@ -38,12 +60,95 @@ static void player2turn(List<char>board)
     Console.WriteLine("o's turn to choose a square (1-9): ");
     int turn = int.Parse(Console.ReadLine());
     updateboard(board,turn,player2);
+}
+static int checkWinner(List<char>board)
+{
+     if (board[0] == board[1] && board[1] == board[2])
+            {
+                return 1;
+            }
+            else if (board[3] == board[4] && board[4] == board[5])
+            {
+                return 1;
+            }
+            else if (board[6] == board[7] && board[7] == board[8])
+            {
+                return 1;
+            }
+            else if (board[0] == board[3] && board[3] == board[6])
+            {
+                return 1;
+            }
+            else if (board[1] == board[4] && board[4] == board[7])
+            {
+            return 1;
+            }
+            else if (board[2] == board[5] && board[5] == board[8])
+            {
+                return 1;
+            }
+            else if (board[0] == board[4] && board[4] == board[8])
+            {
+                return 1;
+            }
+            else if (board[2] == board[4] && board[4] == board[6])
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+}
 
+static int checktie(List<char>board)
+{
+  if (board[0] != '1' && board[1] != '2' && board[2] != '3' && board[3] != '4' && board[4] != '5' && board[5] != '6' && board[6] != '7' && board[7] != '8' && board[8] != '9')
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }               
 }
 static void updateboard(List<char>board, int turn, char player )
 {
- board[turn-1] = player;
- displayboard(board);  
+if (board[turn-1] !='o')
+{
+    if (board[turn-1] !='x')
+    {
+        board[turn-1] = player;
+        displayboard(board);
+ 
+    }
+    else
+    {
+        Console.WriteLine("Sorry that square is already taken, please try again");
+        if (player =='x')
+        {
+            player1turn(board);
+        }
+        else
+        {
+            player2turn(board);
+        }   
+    }
+
 }
+else
+{
+    Console.WriteLine("Sorry that square is already taken, please try again");
+    if (player =='x')
+    {
+        player1turn(board);
+    }
+    else
+    {
+        player2turn(board);
+    }   
+}
+
+    }
     }
 }
